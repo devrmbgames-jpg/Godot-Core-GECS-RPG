304d5ee731be5fbfae7b6542ded6d2229866488f
Valeryn4 <dtho47@gmail.com>
2026-08-27 01:18:55 +1000

    add debug

--- content/ecs/entities/character/e_kinematic_character.gd
+++ content/ecs/entities/character/e_kinematic_character.gd
@@ -10,7 +10,7 @@ func define_components() -> Array:
 		C_InteractionSensor.new(), C_AIInteractionGoal.new(), C_CombatState.new(),
 		C_MotorState.new(), C_ExternalMotion.new(),
 		C_MoveSpeed.new(), C_Acceleration.new(), C_Deceleration.new(), C_TurnSpeed.new(), C_Gravity.new(), C_InteractionRange.new(),
-		C_Health.new(), C_MaxHealth.new(), C_Mana.new(), C_MaxMana.new(),
+		C_Health.new(100000), C_MaxHealth.new(100000), C_Mana.new(), C_MaxMana.new(),
 		C_Damage.new(), C_Armor.new(), C_AttackSpeed.new(), C_CastSpeed.new(),
 		C_CooldownRecovery.new(), C_ManaCostMultiplier.new(),
 		C_AbilityQueue.new(), C_Casting.new(), C_CombatTarget.new(), C_Inventory.new(), C_Team.new(), C_StatsDirty.new(),
